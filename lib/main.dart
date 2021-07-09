ce5ac1484ad70185e35def07cb2d40c170af1c6b
Vanshaj Bhati <vanshajbhati@Vanshajs-MacBook-Air.local>
2021-07-09 17:37:53 +0530

    spotify clone susing flutter

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
+
+
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+
+    );
+  }
+}
+
